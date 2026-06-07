5aee440e00bb42240b4723ed21ba4dd38e4335ab
Mohamed Dernoun <med.dernoun@gmail.com>
2026-06-07 17:34:33 +0100

    Use object instead of the soqp envelope

--- test/net_test.dart
+++ test/net_test.dart
@@ -5,11 +5,6 @@ import 'package:idempierews_dart/src/request.dart';
 import 'package:test/test.dart';
 
 void main() {
-  String dataRequest =
-      '<soapenv:Envelope xmlns:_0="http://idempiere.org/ADInterface/1_0" xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"><soapenv:Header/><soapenv:Body><_0:createUpdateData><_0:ModelCRUDRequest><_0:ModelCRUD><_0:serviceType>CreateUpdateUser</_0:serviceType><_0:DataRow><_0:field column="Name"><_0:val>Joe Block</_0:val></_0:field><_0:field column="Phone"><_0:val>555-879-89</_0:val></_0:field><_0:field column="C_BPartner_ID"><_0:val>118</_0:val></_0:field><_0:field column="EMail"><_0:val>joeblock@gardenworld.com</_0:val></_0:field></_0:DataRow></_0:ModelCRUD><_0:ADLoginRequest><_0:user>SuperUser</_0:user><_0:pass>System</_0:pass><_0:ClientID>11</_0:ClientID><_0:RoleID>50004</_0:RoleID><_0:OrgID>11</_0:OrgID><_0:WarehouseID>103</_0:WarehouseID><_0:stage>2</_0:stage></_0:ADLoginRequest></_0:ModelCRUDRequest></_0:createUpdateData></soapenv:Body></soapenv:Envelope>';
-  // XmlDocument xmlDocumentRequest = XmlDocument.parse(dataRequest);
-  String dataResponse =
-      '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><ns1:createUpdateDataResponse xmlns:ns1="http://idempiere.org/ADInterface/1_0"><StandardResponse xmlns="http://idempiere.org/ADInterface/1_0" RecordID="1000006"></StandardResponse></ns1:createUpdateDataResponse></soap:Body></soap:Envelope>';
   // XmlDocument xmlDocument = XmlDocument.parse(dataResponse);
   test('Test the return of the web service', () async {
     WebServiceConnection webServiceConnection = WebServiceConnection();
@@ -21,8 +16,8 @@ void main() {
         'https://demo.globalqss.com/ADInterface/services/ModelADService';
 
     LoginRequest _login = new LoginRequest();
-    _login.setUser = 'WebService';
-    _login.setPass = 'WebService';
+    _login.setUser = 'SuperUser';
+    _login.setPass = 'System';
     _login.setClientID = 11;
     _login.setRoleID = 50004;
     _login.setOrgID = 11;
@@ -39,8 +34,8 @@ void main() {
     data.addField('C_BPartner_ID', '118');
     data.addField('EMail', 'joeblock@gardenworld.com');
     createData.setDataRow = data;
-    expect(
-        (await webServiceConnection.sendStringRequest(dataRequest)).toString(),
-        dataResponse);
+    WebServiceResponse webServiceResponse =
+        await webServiceConnection.sendRequest(createData);
+    expect(webServiceResponse.getStatus, WebServiceResponseStatus.Successful);
   });
 }
